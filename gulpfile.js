9a7e18b732cf4c28b2285b39ad839807c8deaf77
esimms311 <eric.simmons26@gmail.com>
2017-04-07 22:01:07 -0600

    my wife needs me

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,7 @@ var babel = require('gulp-babel');
 var plumber = require('gulp-plumber');
 var sourcemaps = require('gulp-sourcemaps');
 var watch = require('gulp-watch');
-var nodemailer = require('nodemailer');
+// var nodemailer = require('nodemailer');
 
 const paths = {
   scssSource: './public/styles/**/*.scss',
@@ -13,3 +13,34 @@ const paths = {
   jsSource: ['./public/js/app.js', './public/js/**/*.js'],
   jsDest: './public/compiled/js'
 };
+gulp.task('styles', () => {
+  return gulp.src(paths.scssSource)
+  .pipe(sass().on('error', sass.logError))
+  .pipe(concat('styles.css'))
+  .pipe(gulp.dest(paths.scssDest));
+});
+gulp.task('frontjs', () => {
+  return gulp.src(paths.jsSource)
+  .pipe(sourcemaps.init())
+  .pipe(plumber())
+  .pipe(babel({
+    presets: ["es2015"]
+  }))
+  .pipe(concat('bundle.js'))
+  .pipe(sourcemaps.write())
+  .pipe(gulp.dest(paths.jsDest));
+});
+gulp.task('watch', () =>  {
+  gulp.watch(paths.jsSource, ['frontjs']);
+  gulp.watch(paths.scssSource, ['styles']);
+});
+gulp.task('watch', () => {
+  watch(paths.jsSource, {usePolling:true}, () => {
+    gulp.start('frontjs')
+  })
+  watch(paths.scssSource, {usePolling:true}, () => {
+    gulp.start('styles')
+  })
+})
+
+gulp.task('default', ['watch', 'frontjs', 'styles']);
---
+++ public/JS/directives/navbar.js
@@ -0,0 +1,9 @@
+angular.module('gopro').directive('navbar', () => {
+
+  return {
+    templateUrl: './templates/navbar.html',
+    controller: function($state, $rootScope, $scope, homeSrvc) {
+      
+    }
+  }
+})
