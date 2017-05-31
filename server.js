a2d7e8c91d359a6173b4edbcb0c17b14eea1193c
esimms311 <eric.simmons26@gmail.com>
2017-05-31 14:48:18 -0600

    fixing r10 error

--- server.js
+++ server.js
@@ -9,13 +9,21 @@ var port = 4000;
 
 var app = module.exports = express();
 // var app = express();
+app.set('port', (process.env.PORT || 5000))
 app.use(bodyParser.json());
 app.use(express.static(__dirname + '/public'));
 app.use(cors());
 
+app.get('/', function(request, response) {
+    var result = 'App is running'
+    response.send(result);
+}).listen(app.get('port'), function() {
+    console.log('App is running, server is listening on port ', app.get('port'));
+});
+
 
 // app.get('/home', productsController.getHome);
 
-app.listen(port, function(){
-  console.log('connected on ', port)
-});
+// app.listen(port, function(){
+//   console.log('connected on ', port)
+// });
